2adf18c6eef4880eaaaf9d0fa23ac3bb13019a55
iuneFox-here <andoryuuriemu@gmail.com>
2026-06-06 16:13:45 +0900

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyADoDHS9RR-x7Bx_lgWKi8jFGl__W0OGbc",
+  authDomain: "to-do-list-df9cb.firebaseapp.com",
+  projectId: "to-do-list-df9cb",
+  storageBucket: "to-do-list-df9cb.firebasestorage.app",
+  messagingSenderId: "230771165648",
+  appId: "1:230771165648:web:e1b50da1b9d46f9c3091d5"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
